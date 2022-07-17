3a76aa8eba044e4c6537cfe6e89325bd092e14c9
artem <arti_man1997@mail.ru>
2022-07-18 01:35:13 +0300

    7-8 лаба

--- src/Model/userDTO.php
+++ src/Model/userDTO.php
@@ -3,11 +3,17 @@
 namespace App\Model;
 
 use JMS\Serializer\Annotation as Serializer;
+use OpenApi\Annotations as OA;
 use Symfony\Component\Validator\Constraints as Assert;
 
 class userDTO
 {
     /**
+     * @OA\Schema(
+     * title="UserDTO",
+     * description="UserDTO"
+     * )
+     *
      * @Serializer\Type("string")
      * @Assert\Email(message="Email address {{ value }} is not valid")
      */
@@ -22,5 +28,77 @@ class userDTO
      * @Assert\NotBlank()
      */
     public $password;
-    //...
+    /**
+     * @OA\Property(
+     *     type="array",
+     *     @OA\Items(
+     *         type="string"
+     *     ),
+     *     title="Roles",
+     *     description="Roles"
+     * )
+     * @Serializer\Type("array")
+     */
+    public $roles = [];
+
+    /**
+     * @OA\Property(
+     *     type="float",
+     *     title="Balance",
+     *     description="Balance"
+     * )
+     * @Serializer\Type("float")
+     */
+    public $balance;
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function setEmail($email): void
+    {
+        $this->email = $email;
+    }
+
+    public function setPassword($password): void
+    {
+        $this->password = $password;
+    }
+
+    public function getRoles(): array
+    {
+        return $this->roles;
+    }
+
+    public function setRoles(array $roles): void
+    {
+        $this->roles = $roles;
+    }
+
+    public function getBalance()
+    {
+        return $this->balance;
+    }
+
+    public function setBalance(float $balance): void
+    {
+        $this->balance = $balance;
+    }
+
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): void
+    {
+        $this->username = $username;
+        $this->email = $username;
+    }
 }
